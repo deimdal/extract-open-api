46d06963749e5c16a986229456cdfa6b06da37b6
Dmitry <dmitry@deimdal.com>
2026-04-09 23:37:34 +0200

    Add support for OpenApi response schemas

--- Deimdal.ExtractOpenApi/OpenApiDocumentModifier.cs
+++ Deimdal.ExtractOpenApi/OpenApiDocumentModifier.cs
@@ -1,17 +1,99 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.OpenApi;
 using Microsoft.OpenApi.Extensions;
+using Microsoft.OpenApi.Readers;
 using Microsoft.OpenApi.Models;
 
 namespace Deimdal.ExtractOpenApi;
 
 public static class OpenApiDocumentModifier
 {
+    private static readonly Lazy<HttpClient> Client = new(() => new());
+
+    /// <summary>
+    /// Process local or remote document
+    /// </summary>
+    /// <param name="source">Source document URL or filename</param>
+    /// <param name="destinationFile">Destination file</param>
+    /// <param name="pathFilter">Path to extract</param>
+    /// <param name="destinationVersion">Target document version</param>
+    /// <param name="destinationFormat">Target document format</param>
+    public static async Task ProcessDocument(
+        string source,
+        FileInfo destinationFile,
+        string[] pathFilter,
+        OpenApiSpecVersion destinationVersion,
+        OpenApiFormat destinationFormat)
+    {
+        Stream? stream = null;
+        try
+        {
+            var paths = new Dictionary<string, string[]?>();
+            foreach (var pathFilterSpec in pathFilter)
+            {
+                var parts = pathFilterSpec.Split('=');
+                if (parts.Length > 2)
+                    throw new Exception($"Invalid path filter specification: '{pathFilterSpec}'. Expected: path[=operation1[,operation2,...]]");
+                var methods = parts.Length == 2 ? parts[1].Split(',') : null;
+                if (paths.ContainsKey(parts[0]))
+                    throw new Exception($"Duplicate path filter specification: '{parts[0]}' in '{pathFilterSpec}'.");
+                paths.Add(parts[0], methods);
+            }
+
+            if (Uri.IsWellFormedUriString(source, UriKind.Absolute))
+            {
+                Console.WriteLine($"Downloading '{source}'...");
+                stream = await Client.Value.GetStreamAsync(source);
+            }
+            else
+            {
+                if (!File.Exists(source))
+                    throw new FileNotFoundException($"File '{source}' not found.");
+                stream = new FileStream(source, FileMode.Open, FileAccess.Read, FileShare.Read);
+            }
+
+            var doc = new OpenApiStreamReader().Read(stream, out var diagnostic);
+            await stream.DisposeAsync();
+            stream = null;
+
+            if (diagnostic.Errors.Any())
+            {
+                Console.WriteLine($"Errors in OpenApi document '{source}': {string.Join(", ", diagnostic.Errors)}");
+                Console.WriteLine("This is a warning message only. Processing is being continued.");
+            }
+
+            Modify(doc, paths);
+
+            if (destinationFile.Exists)
+                destinationFile.Delete();
+
+            await using var fileStream = destinationFile.OpenWrite();
+            doc.Serialize(fileStream, destinationVersion, destinationFormat);
+            await fileStream.FlushAsync();
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            throw;
+        }
+        finally
+        {
+            if (stream != null)
+                await stream.DisposeAsync();
+        }
+
+        Console.WriteLine("Process is completed.");
+    }
+
     /// <summary>
     /// Filter OpenApi document operations and their schemes
     /// </summary>
-    public static void Modify(OpenApiDocument doc, Dictionary<string, string[]?> paths)
+    private static void Modify(OpenApiDocument doc, Dictionary<string, string[]?> paths)
     {
         var notFound = paths.Where(x => !doc.Paths.ContainsKey(x.Key)).ToList();
         if (notFound.Count > 0)
@@ -59,6 +141,7 @@ public static class OpenApiDocumentModifier
     private static void ShakeOperationsSchemaTree(OpenApiDocument doc)
     {
         var persistSchemas = new HashSet<OpenApiSchema>();
+        var persistResponses = new HashSet<OpenApiResponse>();
         foreach (var (_, pathItem) in doc.Paths)
         {
             foreach (var (_, operation) in pathItem.Operations)
@@ -75,9 +158,14 @@ public static class OpenApiDocumentModifier
                         CheckSchemaExtractable(contentValue.Schema, persistSchemas);
 
                 foreach (var (_, openApiResponse) in operation.Responses)
+                {
+                    persistResponses.Add(openApiResponse);
                     if (openApiResponse.Content != null)
                         foreach (var (_, contentValue) in openApiResponse.Content)
+                        {
                             CheckSchemaExtractable(contentValue.Schema, persistSchemas);
+                        }
+                }
             }
         }
 
@@ -87,12 +175,23 @@ public static class OpenApiDocumentModifier
                 continue;
             doc.Components.Schemas.Remove(key);
         }
+
+        foreach (var (key, value) in doc.Components.Responses.ToList())
+        {
+            if (persistResponses.Contains(value))
+                continue;
+            doc.Components.Responses.Remove(key);
+        }
     }
 
     private static void CheckSchemaExtractable(OpenApiSchema? schema, HashSet<OpenApiSchema> rootSchemas)
     {
         if (schema is { Reference.Id: not null })
+        {
             ProcessPropertiesIfNew(schema, rootSchemas);
+            if (schema is { Items.Reference: not null })
+                ProcessPropertiesIfNew(schema.Items, rootSchemas);
+        }
         else if (schema is { Items.Reference: not null })
             ProcessPropertiesIfNew(schema.Items, rootSchemas);
         else
